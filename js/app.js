a96942f394e47d5245e7122181309180dd707a01
MateusSilva <mateus_silva139@hotmail.com>
2022-12-10 02:44:47 -0300

    terceiro commit

--- js/app.js
+++ js/app.js
@@ -2,18 +2,20 @@ function calcularIMC() {
   //ALTURA DIVIDIDO POR 100//
   const alturaNumber = document.getElementById("altura-number").value / 100;
   const pesoNumber = document.getElementById("peso-number").value;
-  const resultado = pesoNumber / alturaNumber ** 2;
+  const resultado = (pesoNumber / alturaNumber ** 2);
   const txtResult = document.getElementById("txt-result");
   const txtResultado = document.getElementById("txt-resultado")
-  if (resultado < 18.5) {
+
+
+  
+
+if (resultado < 18.5) {
     txtResult.innerHTML = "Abaixo do peso normal";
   } else if (resultado < 24.9) {
     txtResult.style.animationPlayState = "paused";
     txtResult.style.opacity="100"
     txtResult.innerHTML = "Peso normal";
     txtResult.style.color = "Lime";
-
-    console.log(txtResultado);
   } else if (resultado < 29.9) {
     txtResult.innerHTML = "Excesso de peso";
     txtResult.style.color = "GreenYellow";
@@ -23,9 +25,23 @@ function calcularIMC() {
   } else if (resultado < 39.9) {
     txtResult.innerHTML = "Obesidade classe 2";
     txtResult.style.color = "MediumVioletRed";
-  } else if ((resultado) => 40) {
+  } else if (resultado => 40) {
     txtResult.innerHTML = "Obesidade classe 3";
     txtResult.style.color = "DarkRed";
   }
-  console.log(resultado);
+
+  number =NaN
+  if (isNaN(resultado)) number = 0
+
+  if(number==0){
+    const resultado=number
+    txtResult.innerHTML = "Insira um valor válido!";
+    alert("VALORES INVÁLIDOS")
+    console.log(resultado)
+
+  }else {
+
+    console.log(resultado)
+  }
+
 }
